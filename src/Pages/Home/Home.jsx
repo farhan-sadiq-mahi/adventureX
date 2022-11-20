8ddd61f378f77f5da59b56ffca340aeed85988d8
farhan-sadiq-mahi <fsmahi100@gmail.com>
2022-11-20 17:33:56 +0600

    navbar added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../../SharedComponent/Navbar';
+import HeroComponent from './HeroComponent';
 
 const Home = () => {
     return (
         <div>
             <Navbar />
+            <HeroComponent />
 
         </div>
     );
